dcf4108d58cafddead4f3db0e1d2ba22cceb65f5
rmuhamed <ricardo.muhamed@gmail.com>
2019-07-21 11:23:21 +0200

    Login improvements -> Check username API configuration hidden inside gradle module configuration

--- app/src/main/java/com/rmuhamed/sample/myselfiesapp/api/ImgurAPI.kt
+++ app/src/main/java/com/rmuhamed/sample/myselfiesapp/api/ImgurAPI.kt
@@ -1,19 +1,17 @@
 package com.rmuhamed.sample.myselfiesapp.api
 
+import com.rmuhamed.sample.myselfiesapp.api.dto.BasicResponseDTO
 import com.rmuhamed.sample.myselfiesapp.api.dto.TokenRequestDTO
 import com.rmuhamed.sample.myselfiesapp.api.dto.TokenResponseDTO
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface ImgurAPI {
     @POST("/oauth2/token")
     fun createToken(@Body tokenRequest : TokenRequestDTO) : Call<TokenResponseDTO>
 
-    @GET("/3/account/{username}")
-    fun verifyAccount(@Path("userName") userName : String)
+    @GET("/3/account/{userName}/verifyemail")
+    fun verifyAccount(@Header("Authorization") authHeader: String, @Path("userName") userName: String): Call<BasicResponseDTO>
 
     @GET("/3/album/{albumHash}/images")
     fun getImages(@Path("albumHash") albumHash: String)
